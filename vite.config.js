40f84893491de07655a2c97ed19205adfb347942
ImadEhsan <imadehsan1@gmail.com>
2025-03-14 10:48:22 +0500

    vercel changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//     react()],
+// })
+import tailwindcss from '@tailwindcss/vite'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
-})
+  plugins: [react(),
+    tailwindcss()
+  ],
+
+  base: "./",  // 🔹 Ensures correct asset paths for Vercel
+  build: {
+    outDir: "dist", // 🔹 Ensure Vite builds to "dist"
+  }
+});
